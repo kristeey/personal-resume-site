68b0edcc8718bcd6584c6c4ed276a1218cc67c48
Kristian <krstene94@gmail.com>
2023-01-20 00:20:46 +0100

    Migrate from CRA to vite and fix security vulnerabilities

--- app/src/components/Template/SideBar.jsx
+++ app/src/components/Template/SideBar.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 
 import ContactIcons from '../Contact/ContactIcons';
 
-const { PUBLIC_URL } = process.env; // set automatically from package.json:homepage
+const { PUBLIC_URL } = import.meta.env; // set automatically from package.json:homepage
 
 const SideBar = () => (
   <section id="sidebar">
     <section id="intro">
       <Link to="/" className="logo">
-        <img src={`${PUBLIC_URL}/images/me.png`} alt="" />
+        <img src={`/images/me.png`} alt="" />
       </Link>
       <header>
         <h2>Kristian Sørensen Stene</h2>
