1d19e7cfe83a5ef566e6b5e88b5d9991324f59de
Kristian <krstene94@gmail.com>
2023-03-15 11:15:33 +0100

    Add GAR read permission to cluster default service account

--- terraform/gar.tf
+++ terraform/gar.tf
@@ -10,3 +10,11 @@ resource "github_actions_secret" "gar-repo-name" {
   secret_name      = "GAR_REPO_NAME"
   plaintext_value  = google_artifact_registry_repository.personal-resume-repo.repository_id
 }
+
+## Give cluster default SA read permission on GAR
+# to let workloads in the cluster pull images from here.
+resource "google_project_iam_member" "gar-reader" {
+  project = var.project_id
+  role    = "roles/artifactregistry.reader"
+  member  = "serviceAccount:${google_container_cluster.resume_cluster.node_pool[0].node_config[0].service_account}"
+}
